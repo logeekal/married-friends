8f8828654cbc5bfb43aed6d11c34c23610bbc4f6
Jatin Kathuria <jtn.kathuria@gmail.com>
2021-05-23 23:39:47 +0000

    updated google analytics with plugin

--- packages/mf-blog-theme/gatsby-ssr.js
+++ packages/mf-blog-theme/gatsby-ssr.js
@@ -16,17 +16,6 @@ export const onRenderBody = ({ setPostBodyComponents }) => {
       type="module"
       src="https://cdn.jsdelivr.net/npm/@justinribeiro/lite-youtube@0.6.2/lite-youtube.js"
       async
-    />,
-
-    <script
-      async
-      src="https://www.googletagmanager.com/gtag/js?id=UA-24329901-2"
-    />,
-
-    <script>
-      window.dataLayer = window.dataLayer || []; function gtag()
-      {dataLayer.push(arguments)}
-      gtag('js', new Date()); gtag('config', 'UA-24329901-2');
-    </script>,
+    />
   ]);
 };
